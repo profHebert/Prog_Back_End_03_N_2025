5ed49d08823fc2afc43c88125ecaf254376cb113
Guilherme Turra <g8turra@gmail.com>
2025-09-09 20:05:39 -0300

    Add aula05

---
+++ aula05_01.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula05_01</title>
+</head>
+<body>
+    <h1>Aula05_01 - 09/09/2025</h1>
+    
+</body>
+</html>
