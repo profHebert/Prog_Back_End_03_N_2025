11bc273cd15632c14e0b3f82f1ed8afe13ca0663
Guilherme Turra <g8turra@gmail.com>
2025-09-30 23:35:39 +0000

    Add aula08

---
+++ aula08_01.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula08_01</title>
+</head>
+<body>
+    <h1>Aula08_01</h1>
+    <form method="post" action="">
+        Digite sua idade: <input type="number" name="idade" required><br>
+        <input type="submit" value="Enviar!"><br>
+    </form>
+    
+</body>
+</html>
+
+<?php
+    if(count($_POST) == 0) exit; // SE NÃO TIVER NADA NO POST, SAI!!!
+    $idade = $_POST["idade"];
+
+    echo "Idade digitada: <b>$idade<b>.";
+    
+    if ($idade < 13) {
+        echo "Você não pode criar uma conta. A idade mínima é de 13 anos."
+    } else if($idade >= 14 && $idade <= 18) {
+        echo "Você pode criar uma conta com consentimento dos pais."
+    } else if($idade >= 18 && $idade <= 64) {
+        echo "Você pode criar uma conta. Bem vindo á nossa configuração."
+    } else if($idade >= 65) {
+        echo "Você não pode criar uma conta. Lembre-se de verificar nossa configuração."
+    } else {
+        echo "Idade inválida!!!"
+    }
+?>
